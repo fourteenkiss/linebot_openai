aeb1efc4910fd9b08a2c76355c405b5037123671
fourteenkiss <n7850204@gmail.com>
2024-06-28 14:37:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def GPT_response(text):
     # 接收回應
-    response = openai.Completion.create(model="gpt-4o-2024-05-13", prompt=text, temperature=0.5, max_tokens=500)
+    response = openai.ChatCompletion.create(model="gpt-4o-2024-05-13", prompt=text, temperature=0.5, max_tokens=500)
     print(response)
     # 重組回應
     answer = response['choices'][0]['text'].replace('。','')
